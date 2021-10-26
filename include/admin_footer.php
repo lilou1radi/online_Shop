10361e70a1d49e621d63d0f1e115e7e5838abc39
lilou1radi <62358941+lilou1radi@users.noreply.github.com>
2021-10-26 22:31:22 +0100

    Add files via upload

---
+++ include/admin_footer.php
@@ -0,0 +1,11 @@
+ <div id="footer">
+                    
+            <ul class="footer_nav">
+                <li style="float:left"><img id="foot" src="img/l.png"> </li>
+                <li>E-mail: test@gmail.co.ma.</li>
+                <li>Téléphone : +212 522000000</li>
+                <li>Adresse : 1, rue xyz Casablanca, Maroc</li>
+            </ul>
+     
+        </div>
+    
---
+++ include/footer.php
@@ -0,0 +1,11 @@
+ <div id="footer">
+                    
+            <ul class="footer_nav">
+                <li style="float:left"><img id="foot" src="img/l.png"> </li>
+                <li><a href="#">E-mail: test@gmail.co.ma.</a></li>
+                <li><a href="#">Adresse : 1, rue xyz, Casablanca, Maroc</a></li>
+                <li><a href="#">Téléphone : +212 5220-0000</a></li>
+            </ul>
+     
+        </div>
+    
